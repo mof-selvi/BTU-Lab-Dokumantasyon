3413ca2a3bb107cb051fb4a5e633cb21d70a6cf8
Muhammed Ömer Faruk Selvi <58203457+mof-selvi@users.noreply.github.com>
2024-02-08 15:21:49 +0300

    Add files via upload

--- scripts-2024/WIN-notepadpp.sh
+++ scripts-2024/WIN-notepadpp.sh
@@ -1,11 +1,13 @@
-LABNO=1
+rm -f .ssh/known_hosts
+LABNO=3
 MAXPC=53
 if [[ $LABNO -eq 3 ]]
     then
         MAXPC=49
 fi
-for (( i = 0; i <= $MAXPC; i++ ))
+for (( i = 1; i <= $MAXPC; i++ ))
 do
 echo "Processing pc ${i}"
 sshpass -p 'btubm2023' ssh -o StrictHostKeyChecking=no "lab${LABNO}pc${i}.local" 'choco install notepadplusplus -y'
 done
+
