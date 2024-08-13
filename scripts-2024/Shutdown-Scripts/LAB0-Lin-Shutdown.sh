aec1968e086f50a97f3b0878682f2e257a323897
Muhammed Ömer Faruk Selvi <58203457+mof-selvi@users.noreply.github.com>
2024-08-13 18:00:10 +0300

    Add files via upload

---
+++ scripts-2024/Shutdown-Scripts/LAB0-Lin-Shutdown.sh
@@ -0,0 +1,8 @@
+rm -f .ssh/known_hosts
+LABNO=0
+MAXPC=55
+for (( i = 1; i <= $MAXPC; i++ ))
+do
+echo "Processing pc ${i}"
+sshpass -p 'btubm2024' ssh -o StrictHostKeyChecking=no "lab${LABNO}pc${i}.local" 'echo btubm2024 | sudo -S shutdown now' & 
+done
