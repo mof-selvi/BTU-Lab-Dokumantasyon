0a4133b17dc4addd19910c17deec022d7767d346
Muhammed Ömer Faruk Selvi <58203457+mof-selvi@users.noreply.github.com>
2024-08-13 17:59:13 +0300

    Delete scripts-2024/Shutdown-Scripts directory

--- scripts-2024/Shutdown-Scripts/LAB1-Shutdown-Lin.sh
+++
@@ -1,8 +0,0 @@
-rm -f .ssh/known_hosts
-LABNO=1
-MAXPC=55
-for (( i = 1; i <= $MAXPC; i++ ))
-do
-echo "Processing pc ${i}"
-sshpass -p 'btubm2024' ssh -o StrictHostKeyChecking=no "lab${LABNO}pc${i}.local" 'echo btubm2024 | sudo -S shutdown now' & 
-done
